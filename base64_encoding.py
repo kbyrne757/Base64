b6409675f78a5a053d4e09420fdc13d8b674956f
Lemon Zest <k.byrne357@gmail.com>
2021-08-02 16:24:29 +0100

    base64 Decoding

--- base64_encoding.py
+++ base64_encoding.py
@@ -9,3 +9,9 @@ base64Encoding = base64.b64encode(userInputAscii)
 base64Complete = base64Encoding.decode('ascii')
 
 print(base64Complete)
+
+#base64 Decoding
+
+userInputDecode = base64.b64decode(base64Complete)
+userInputyeet = userInputDecode.decode('ascii')
+print (userInputyeet)
