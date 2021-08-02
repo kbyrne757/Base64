76205b5583dcbebdc3d561a2f88c6657497a5613
Lemon Zest <k.byrne357@gmail.com>
2021-08-02 16:20:47 +0100

    base64 encoding

--- base64_encoding.py
+++ base64_encoding.py
@@ -4,3 +4,8 @@
 import base64
 
 userInput = input("Enter string to convert to base64: ")
+userInputAscii = userInput.encode('ascii')
+base64Encoding = base64.b64encode(userInputAscii)
+base64Complete = base64Encoding.decode('ascii')
+
+print(base64Complete)
