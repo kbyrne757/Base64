655d50774a535a6dd87945dcff58b497e5639a79
Lemon Zest <k.byrne357@gmail.com>
2021-08-02 16:33:36 +0100

    Added Uppercase & else

--- base64_encoding.py
+++ base64_encoding.py
@@ -4,22 +4,26 @@
 import base64
 
 decision = input("Would you like to encode or decode Base64? (encode/decode) ")
+decision = decision.upper()
 
-if (decision == 'encode'):
+
+if (decision == 'ENCODE'):
     #base64 Encoding
     userInput = input("Enter string to convert to base64: ")
     userInputAscii = userInput.encode('ascii')
     base64Encoding = base64.b64encode(userInputAscii)
     base64Complete = base64Encoding.decode('ascii')
-
     print(base64Complete)
 
-elif (decision == 'decode'):
+elif (decision == 'DECODE'):
     #base64 Decoding
     userInput = input("Enter string to convert to base64: ")
     userInputDecode = base64.b64decode(userInput)
     userOutput = userInputDecode.decode('ascii')
     print (userOutput)
+
+else:
+    print("Invalid Input")
     
 
 
